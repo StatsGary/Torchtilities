a995e6c152fac92d0c8d54bfd04e9127c23a7efd
Gary Hutson <44023992+StatsGary@users.noreply.github.com>
2021-12-23 14:25:01 +0000

    Uploaded first version to PyPI

--- README.txt
+++ README.txt
@@ -1,1 +1,5 @@
 Utilities for working and converting PyTorch objects.
+
+This package currently includes:
+
+- onnx converter - this is used to convert modules from PyTorch over to Tensorflow model graphs. Making the conversion process easy.
--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
 from setuptools import setup, find_packages
-
+#https://pypi.org/classifiers/
 classifiers = [
     'Development Status :: 5 - Production/Stable',
-    'Intended Audience :: PyTorch Developers',
+    'Intended Audience :: Science/Research',
     'Operating System :: Microsoft :: Windows :: Windows 10',
-    'Licence :: OSI Approved :: MIT License',
-    'Programming Language:: Python :: 3'
+    'Operating System :: MacOS :: MacOS X',
+    'Operating System :: Unix',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.0',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7'
 ]
 
 setup(
     name='torchtilities',
-    version='0.0.1',
+    version='0.0.2',
     description='Utilities for PyTorch and conversion functions',
     long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
     url='https://hutsons-hacks.info/',
